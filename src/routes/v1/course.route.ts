958b87ada1de734950c04c09b1290088c8c5d344
Shreyas4545 <shreyasjakati66@gmail.com>
2025-03-05 18:56:31 +0530

    Changes

--- src/routes/v1/course.route.ts
+++ src/routes/v1/course.route.ts
@@ -20,7 +20,7 @@ import { isLoggedIn } from "../../middlewares/authorization";
 
 router.route("/addCourse").post(isLoggedIn, addCourse);
 router.route("/updateCourse/:id").put(isLoggedIn, updateCourse);
-router.route("/getCourseId").get(isLoggedIn, getCourseId);
+router.route("/getCourseId").get(getCourseId);
 router.route("/getCourses").get(getCoursesWithSubjectsAndLectures);
 router.route("/getHomeCourses").get(getHomeCourses);
 router.route("/getWebHomeCourses").get(getWebHomeCourses);
